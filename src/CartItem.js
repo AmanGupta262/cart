f2d53658a06da7117fc7c457d626286c899599cd
Aman Gupta <amang5840@gmail.com>
2021-04-30 16:42:32 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,67 +1,83 @@
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: "Phone",
-            qty: 2,
-            img: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 2,
+      img: "",
+    };
+  }
 
-    increaseQty = () => {
-        console.log(this);
-    }
-    render() {
-        const { price, title, qty, img } = this.state;
-        return (
-          <div className="cart-item">
-            <div className="left-block">
-              <img src="{img}" alt="" style={styles.image} />
-            </div>
-            <div className="right-block">
-              <div style={{ fontSize: 25 }} className="title">
-                {title}
-              </div>
-              <div style={{ color: "#777" }} className="price">
-                Rs: {price}
-              </div>
-              <div style={{ color: "#777" }} className="quantity">
-                Qty: {qty}
-              </div>
-              <div className="actions">
-                <img
-                  alt="increase"
-                  src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1619690865~hmac=df93efb77eda1fca649cbaa5eff36162"
-                  className="action-icons"
-                  onClick={this.increaseQty}
-                />
-                <img
-                  alt="decrease"
-                  src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1619691034~hmac=9d21b83e3bdf1a3710d413e0845922d5"
-                  className="action-icons"
-                />
-                <img
-                  alt="delete"
-                  src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1619691099~hmac=32c7b8ed6f73ffc289b22163dcc6f053"
-                  className="action-icons"
-                />
-              </div>
-            </div>
+  increaseQty = () => {
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1,
+      };
+    });
+  };
+  decreaseQty = () => {
+    const { qty } = this.state;
+
+    if (qty < 1) return;
+
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1,
+      };
+    });
+  };
+  render() {
+    const { price, title, qty, img } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img src="{img}" alt="" style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }} className="title">
+            {title}
           </div>
-        );
-    }
+          <div style={{ color: "#777" }} className="price">
+            Rs: {price}
+          </div>
+          <div style={{ color: "#777" }} className="quantity">
+            Qty: {qty}
+          </div>
+          <div className="actions">
+            <img
+              alt="increase"
+              src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1619690865~hmac=df93efb77eda1fca649cbaa5eff36162"
+              className="action-icons"
+              onClick={this.increaseQty}
+            />
+            <img
+              alt="decrease"
+              src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1619691034~hmac=9d21b83e3bdf1a3710d413e0845922d5"
+              className="action-icons"
+              onClick={this.decreaseQty}
+            />
+            <img
+              alt="delete"
+              src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1619691099~hmac=32c7b8ed6f73ffc289b22163dcc6f053"
+              className="action-icons"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        backgroundColor: '#ccc'
-    },
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    backgroundColor: "#ccc",
+  },
 };
 
 export default CartItem;
