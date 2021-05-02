0c9d8c092f39b88a0dee7f328a91576e1029c6d7
Aman Gupta <amang5840@gmail.com>
2021-05-02 15:33:44 +0530

    delete product from cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -22,6 +22,12 @@ class CartItem extends React.Component {
   };
   render() {
     const { price, title, qty, img } = this.props.product;
+    const {
+      product,
+      onIncreaseQuantity,
+      onDecreaseQuantity,
+      onDeleteProduct,
+    } = this.props;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -42,18 +48,19 @@ class CartItem extends React.Component {
               alt="increase"
               src="https://www.flaticon.com/svg/vstatic/svg/1828/1828926.svg?token=exp=1619690865~hmac=df93efb77eda1fca649cbaa5eff36162"
               className="action-icons"
-              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1619691034~hmac=9d21b83e3bdf1a3710d413e0845922d5"
               className="action-icons"
-              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1619691099~hmac=32c7b8ed6f73ffc289b22163dcc6f053"
               className="action-icons"
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
