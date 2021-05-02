4b628da990250c5208c7b307dcf691d46c5d5aa2
Aman Gupta <amang5840@gmail.com>
2021-05-02 13:02:35 +0530

    create Cart component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import CartItem from './CartItem'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <CartItem />
   </React.StrictMode>,
   document.getElementById('root')
 );
