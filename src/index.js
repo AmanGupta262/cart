b6fb071512e0c2c87014dd9745bed46f37961c72
Aman Gupta <amang5840@gmail.com>
2021-05-07 22:47:21 +0530

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+require('dotenv').config();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +13,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 reportWebVitals();
